454d13f6680e28911419c64232387ca8de9384c8
Isaiah Harville <harvillerisaiah@gmail.com>
2024-02-27 20:40:01 -0600

    yeah babbbyyyyyy

--- Now Dribble/Now Dribble/Tab Bar Views/TrainNowView.swift
+++ Now Dribble/Now Dribble/Tab Bar Views/TrainNowView.swift
@@ -32,11 +32,40 @@ struct TrainNowView: View {
         UINavigationBar.appearance().shadowImage = UIImage() // Empty image for shadow line
     }
     
+    @StateObject private var viewModel = CategoriesViewModel()
+    
     var body: some View {
-        // TODO
-        // Show categories,
-        // Under categories show the workouts (WorkoutView.swift)
-        
-        Text("WIP")
+        NavigationView {
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.categories) { category in
+                        NavigationLink(destination: CategoryContentsView(categoryId: category.id)) {
+                            Text(category.name)
+                                .padding()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .background(Color.gray)
+                                .cornerRadius(10)
+                        }
+                        .buttonStyle(PlainButtonStyle()) // To ensure the entire row is tappable
+                    }
+                }
+                .padding()
+                .navigationTitle("Categories")
+                .onAppear {
+                    viewModel.fetchCategories()
+                }
+            }
+            .overlay {
+                if viewModel.isLoading {
+                    ProgressView("Loading...")
+                }
+            }
+            .alert("Error", isPresented: Binding<Bool>.constant(viewModel.errorMessage != nil), presenting: viewModel.errorMessage) { errorMessage in
+                Button("OK", role: .cancel) { }
+            } message: { errorMessage in
+                Text(errorMessage)
+            }
+        }
     }
+    
 }
