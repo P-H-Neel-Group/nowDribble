6398c37ea78c7df6aa57550d2d847ce9c7747b0d
Isaiah Harville <harvillerisaiah@gmail.com>
2024-03-16 15:23:51 -0500

    add player

--- Now Dribble/Now Dribble/Video/VideoPlayerView.swift
+++ Now Dribble/Now Dribble/Video/VideoPlayerView.swift
@@ -7,30 +7,75 @@
 
 import SwiftUI
 import AVKit
+import Combine
+
+class VideoPlayerViewModel: ObservableObject {
+    var player = AVPlayer()
+    private var endPlaySubscriber: AnyCancellable?
+    var additionalUrls: [URL]? = nil
+    @Published var currentVideoIndex = 1
+    var shouldPlay: Bool = false // Default value is false
+
+    func setupVideoPlayer(with url: URL) {
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        
+        if shouldPlay{
+            player.play()
+        }
+        
+        endPlaySubscriber = NotificationCenter.default.publisher(for: .AVPlayerItemDidPlayToEndTime, object: playerItem)
+            .sink(receiveValue: { [weak self] _ in
+                self?.videoDidEnd()
+            })
+    }
+
+    private func videoDidEnd() {
+        guard let urls = additionalUrls, currentVideoIndex < urls.count else {
+            return
+        }
+        
+        currentVideoIndex += 1
+        if urls.count > currentVideoIndex-1 {
+            let nextUrl = urls[currentVideoIndex - 1]
+            setupVideoPlayer(with: nextUrl)
+        }
+    }
+}
 
 struct VideoPlayerView: View {
+    @StateObject private var viewModel = VideoPlayerViewModel()
     let url: URL
+    var additionalUrls: [URL]? = nil
     let showCaption: Bool
     let caption: String
+    var shouldPlay: Bool?
+
     private let aspectRatio: CGFloat = 16/9
 
     var body: some View {
         VStack {
             GeometryReader { geometry in
-                 VideoPlayer(player: AVPlayer(url: url))
-                     .frame(width: geometry.size.width, height: geometry.size.width / aspectRatio)
-                     .cornerRadius(10)
-             }
-             .frame(height: UIScreen.main.bounds.width / aspectRatio) // Set height based on the aspect ratio
+                VideoPlayer(player: viewModel.player)
+                    .frame(width: geometry.size.width, height: geometry.size.width / aspectRatio)
+                    .cornerRadius(10)
+                    .onAppear {
+                        viewModel.additionalUrls = additionalUrls
+                        viewModel.shouldPlay = shouldPlay ?? false // Use the shouldPlay value
+                        viewModel.setupVideoPlayer(with: url)
+                        
+                    }
+            }
+            .frame(height: UIScreen.main.bounds.width / aspectRatio)
 
-            if (showCaption) {
+            if showCaption {
                 Text(caption)
                     .font(.caption)
                     .bold()
                     .foregroundColor(.white)
                     .padding([.horizontal, .bottom])
                     .multilineTextAlignment(.leading)
-                    .fixedSize(horizontal: false, vertical: true) // Allows the text to grow vertically.
+                    .fixedSize(horizontal: false, vertical: true)
             }
         }
     }
---
+++ Now Dribble/Now Dribble/Workouts/PlayWorkoutView.swift
@@ -0,0 +1,105 @@
+//
+//  PlayWorkoutView.swift
+//  Now Dribble
+//
+//  Created by Isaiah Harville on 3/16/24.
+//
+
+import SwiftUI
+import AVKit
+
+struct PlayWorkoutView: View {
+    var sequences: [WorkoutSequence]
+    var videos: [WorkoutVideo]
+    @State private var currentSequenceIndex = 0
+    @State private var currentVideoIndex = 0
+    @State private var timerRemainingSeconds: Int
+    @State private var timerIsActive = false
+    
+    private var currentVideoURL: URL {
+        URL(string: videos[currentVideoIndex].url)!
+    }
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    init(sequences: [WorkoutSequence], videos: [WorkoutVideo]) {
+        self.sequences = sequences
+        self.videos = videos
+        _timerRemainingSeconds = State(initialValue: sequences.first?.time ?? 0)
+    }
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: 3)
+                    .foregroundColor(Color("TabButtonColor"))
+                    .frame(width: 200, height: 200)
+                
+                VStack {
+                    Text("\(timerRemainingSeconds)s")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(Color("TabButtonColor"))
+                        .onReceive(timer) { _ in
+                            guard timerIsActive else { return }
+                            if timerRemainingSeconds > 0 {
+                                timerRemainingSeconds -= 1
+                            } else {
+                                moveToNextSequence()
+                            }
+                        }
+                        .onAppear {
+                            startTimer()
+                        }
+                    Text("sec")
+                        .font(.title)
+                        .foregroundColor(Color("TabButtonColor"))
+                }
+            }
+            Text(sequences[currentSequenceIndex].description)
+                .font(.title)
+                .padding([.bottom],10)
+            
+            let initialUrl = URL(string: videos[0].url)!
+            let additionalUrls = videos.dropFirst().compactMap { URL(string: $0.url) }
+            VideoPlayerView(url: initialUrl, additionalUrls: additionalUrls, showCaption: false, caption:"", shouldPlay: true)
+            
+            // Toggle Button for Pause/Resume
+            Button(action: {
+                self.timerIsActive.toggle()
+                if self.timerIsActive {
+                    // Optionally, resume the video if using a video player
+                } else {
+                    // Optionally, pause the video if using a video player
+                }
+            }) {
+                Image(systemName: timerIsActive ? "pause.circle.fill" : "play.circle.fill")
+                    .resizable()
+                    .symbolRenderingMode(.multicolor)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(20) // Adjust padding to fit your design
+                    .font(.system(size: 75))
+                    .foregroundColor(.yellow)
+            }
+        }
+        .onDisappear {
+            timerIsActive = false
+        }
+        .background(Color("PrimaryBlueColor"))
+    }
+    
+    private func startTimer() {
+        timerRemainingSeconds = sequences[currentSequenceIndex].time
+        timerIsActive = true
+    }
+    
+    private func moveToNextSequence() {
+        if currentSequenceIndex < sequences.count - 1 {
+            currentSequenceIndex += 1
+            startTimer()
+        } else {
+            timerIsActive = false
+        }
+    }
+}
