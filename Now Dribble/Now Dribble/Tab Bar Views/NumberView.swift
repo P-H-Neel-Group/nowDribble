c86d823cf78d373ee6a091e5132dddbb61d0a590
Josh Payne <61603116+JayPay108@users.noreply.github.com>
2024-03-20 20:15:27 -0500

    fixed directory name

--- Now Dribble/Now Dribble/Tab Bar Views/NumberView.swift
+++ Now Dribble/Now Dribble/Tab Bar Views/NumberView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct NumberView: View {
     let captions = ["Focus on a firm dibble on the outside of each foot.", "Keep your hand on top of ball to make your dribble strong and quick. Don't go past your center of mass.", "Focus on making your dribble quick and slightly rocking your body towards the direction of the ball, right to left.", "Same as 3, focus on making your dribble quick and slightly rocking your body towards the direction of the ball, right to left (body wrap).", "To speed up ball control, focus on crossing the ball quick and snappy between the center of your wide base without your arms too far outside your waist."]
     
-    let urls = ["https://now-dribble.s3.amazonaws.com/1intro.mp4", "https://now-dribble.s3.amazonaws.com/2intro.mp4", "https://now-dribble.s3.amazonaws.com/3intro.mp4",  "https://now-dribble.s3.amazonaws.com/4intro.mp4",
-        "https://now-dribble.s3.amazonaws.com/5intro.mp4"]
+    let urls = ["https://now-dribble.s3.us-east-2.amazonaws.com/static/1intro.mp4", "https://now-dribble.s3.us-east-2.amazonaws.com/static/2intro.mp4", "https://now-dribble.s3.us-east-2.amazonaws.com/static/3intro.mp4",  "https://now-dribble.s3.us-east-2.amazonaws.com/static/4intro.mp4",
+        "https://static/now-dribble.s3.us-east-2.amazonaws.com/5intro.mp4"]
     
     var body: some View {
         ScrollView {
