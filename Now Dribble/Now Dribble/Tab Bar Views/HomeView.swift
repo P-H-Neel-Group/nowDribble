a54d1d6791c2458937b5f5d4d1b5ded5a65891e0
Isaiah Harville <harvillerisaiah@gmail.com>
2024-04-20 12:43:34 -0500

    done + cleanup of other views

--- Now Dribble/Now Dribble/Tab Bar Views/HomeView.swift
+++ Now Dribble/Now Dribble/Tab Bar Views/HomeView.swift
@@ -28,7 +28,6 @@ struct HomeView: View {
                             PostView(post: post)
                         }
                     }
-
                     .onAppear {
                         viewModel.fetchPosts()
                     }
--- Now Dribble/Now Dribble/Tab Bar Views/TrainNowView.swift
+++ Now Dribble/Now Dribble/Tab Bar Views/TrainNowView.swift
@@ -32,51 +32,9 @@ struct TrainNowView: View {
                             AsyncImage(url: URL(string: category.image_url)) { phase in
                                 switch phase {
                                 case .empty, .failure:
-                                    Rectangle()
-                                        .frame(width: 400, height: 300)
-                                        .cornerRadius(10)
-                                        .foregroundColor(Color("SecondaryBlueColor"))
-                                        .overlay(
-                                            VStack {
-                                                Text(category.name.uppercased())
-                                                    .font(.system(size: 20, weight: .bold, design: .default))
-                                                    .foregroundColor(.white)
-                                                    .padding(5)
-                                                Text("\(category.workout_count) exercises")
-                                                    .font(.caption)
-                                                    .foregroundColor(.white)
-                                            }
-                                        )
-                                        .padding()
-                                case .success(let image):
-                                    image.resizable()
-                                        .frame(maxWidth: 400, maxHeight: 300)
-                                        .scaledToFill()
-                                        .cornerRadius(10)
-                                        //.aspectRatio(contentMode: .fill)
-                                        .clipped()
-                                        .overlay(
-                                            ZStack(alignment: .bottomLeading) { // Align content to the bottom leading corner
-                                                Rectangle() // This rectangle will serve as the tint layer
-                                                    .foregroundColor(.black)
-                                                    .opacity(0.3)
-
-                                                VStack(alignment: .leading) { // Align text to the leading edge
-                                                    Spacer() // Pushes content to the bottom
-                                                    Text(category.name.uppercased())
-                                                        .font(.system(size: 35, weight: .bold, design: .default))
-                                                        .foregroundColor(.white)
-                                                        .padding(.leading, 10) // Add padding to match your design requirements
-                                                    Text("\(category.workout_count) exercises")
-                                                        .font(.system(size: 15, weight: .bold, design: .default))
-                                                        .foregroundColor(.white)
-                                                        .padding([.leading, .bottom], 10) // Align with the title padding
-                                                }
-                                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading) // Ensure VStack fills the ZStack but aligns content to bottomLeading
-                                            }
-                                        )
-                                        //.grayscale(category.user_has_access ? 0 : 1)
-                                        .padding()
+                                    FailView(category: category)
+                                case .success (let image):
+                                    SuccessView(category: category, image: image)
                                 @unknown default:
                                     EmptyView()
                                 }
@@ -104,5 +62,64 @@ struct TrainNowView: View {
             }
         }
         .navigationBarHidden(true)
+    }
+}
+
+struct SuccessView: View {
+    var category: Category
+    var image: Image
+    
+    var body: some View {
+        image.resizable()
+            .frame(maxWidth: 400, maxHeight: 300)
+            .scaledToFill()
+            .cornerRadius(10)
+            //.aspectRatio(contentMode: .fill)
+            .clipped()
+            .overlay(
+                ZStack(alignment: .bottomLeading) { // Align content to the bottom leading corner
+                    Rectangle() // This rectangle will serve as the tint layer
+                        .foregroundColor(.black)
+                        .opacity(0.3)
+                    
+                    VStack(alignment: .leading) { // Align text to the leading edge
+                        Spacer() // Pushes content to the bottom
+                        Text(category.name.uppercased())
+                            .font(.system(size: 35, weight: .bold, design: .default))
+                            .foregroundColor(.white)
+                            .padding(.leading, 10) // Add padding to match your design requirements
+                        Text("\(category.workout_count) exercises")
+                            .font(.system(size: 15, weight: .bold, design: .default))
+                            .foregroundColor(.white)
+                            .padding([.leading, .bottom], 10) // Align with the title padding
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading) // Ensure VStack fills the ZStack but aligns content to bottomLeading
+                }
+            )
+            //.grayscale(category.user_has_access ? 0 : 1)
+            .padding()
+    }
+}
+
+struct FailView: View {
+    var category: Category
+
+    var body: some View {
+        Rectangle()
+            .frame(width: 400, height: 300)
+            .cornerRadius(10)
+            .foregroundColor(Color("SecondaryBlueColor"))
+            .overlay(
+                VStack {
+                    Text(category.name.uppercased())
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .foregroundColor(.white)
+                        .padding(5)
+                    Text("\(category.workout_count) exercises")
+                        .font(.caption)
+                        .foregroundColor(.white)
+                }
+            )
+            .padding()
     }
 }
