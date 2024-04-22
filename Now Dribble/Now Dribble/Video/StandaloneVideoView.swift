e13ae87ac90a75064f29ef9c3c82d16b1afe10c9
Isaiah Harville <harvillerisaiah@gmail.com>
2024-04-21 20:29:37 -0500

    dont want to set up observer -- removing this logic for now

--- Now Dribble/Now Dribble/Video/StandaloneVideoView.swift
+++ Now Dribble/Now Dribble/Video/StandaloneVideoView.swift
@@ -13,20 +13,10 @@ import AVKit
 struct StandaloneVideoView: View {
     let url: String
     let caption: String
-    @State private var isLoading = true  // State to track loading status
 
     var body: some View {
         ZStack {
-            // The video player view
             VideoPlayerView(url: URL(string: url)!, showCaption: true, caption: caption)
-
-            if isLoading {
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle())
-                    .scaleEffect(1.5)  // Increase the size of the spinny thing
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(Color.black.opacity(0.5))
-            }
         }
     }
 }
